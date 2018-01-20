4ec9a5a5b71aa91d6e66d3d0129883c5e3347480
Manuel Ruck <github@manuelruck.de>
2018-01-20 14:33:52 +0100

    prepare android for react-native-navigation

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ import { registerScreens } from "./src/screens";
 
 registerScreens();
 
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: "App",
-      screen: "example.FirstTabScreen", // this is a registered name for a screen
-      title: "Screen One"
-    }
-  ]
+Navigation.startSingleScreenApp({
+  screen: {
+    screen: "example.FirstTabScreen", // unique ID registered with Navigation.registerScreen
+    title: "App" // title of the screen as appears in the nav bar (optional)
+  }
 });
